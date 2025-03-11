1db3141240141c30c291ad4d851ffeb30b992a21
maryam <fatimaisherewithyou@gmail.com>
2025-03-11 18:44:17 +0330

    typeof

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-var age= 22
-var age= 10
+var age = 22;
+var age = 10;
 console.log(age);
 
 // console.log(age + Age);
 
-const number = 10
-const number1 = 15
+const number = 10;
+const number1 = 15;
 console.log(number);
 console.log(age + number);
+
+let name= 'hello"world"'
+console.log(name);
+console.log(name.length);
+
+if(name.length == 12){
+    console.log(true);
+    
+} else{
+    console.log(false);
+    
+}
