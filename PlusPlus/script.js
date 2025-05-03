0482a6368c749830e397ba9d816c18ccefbd6026
maryam <fatimaisherewithyou@gmail.com>
2025-05-04 00:14:47 +0330

    add new practise

---
+++ PlusPlus/script.js
@@ -0,0 +1,24 @@
+// let numbers =[2, 3, 4, 9, 5, 1, 8]
+// let counter = 0;
+// let even = 0;
+
+// for( let i = 0; i<=numbers.length; i++){
+//   if (numbers[i] % 2 ==0){
+//     counter+= numbers[i];
+//   even++;
+//   }
+// }
+// console.log(counter);
+// console.log(even);
+
+// let numbers = [2, 7, 3, 8, 1, 4];
+// let total = 0;
+// let counter = 0;
+
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] % 2 != 0) total += numbers[i];
+//   if (numbers[i] > 5) counter++;
+// }
+
+// console.log(total);
+// console.log(counter);
