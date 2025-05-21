42f4a7a13803495728d6549ff99a98edc7f8d805
maryam <fatimaisherewithyou@gmail.com>
2025-05-21 23:38:30 +0330

    practise

--- Arguments/script.js
+++ Arguments/script.js
@@ -1,3 +1,6 @@
-let newLearn= (...Newarray) => {
-    
+let numbers= (num1, num2, num3 = 0)=> {
+    let numberss= num1 + num2 + num3
+    return numberss;
 }
+console.log(numbers (4,5));
+
---
+++ Hosting/script.js
@@ -0,0 +1,29 @@
+
+// var firstname;
+// console.log(firstname);
+// firstname = 5;
+
+
+// LastName=4;
+// console.log(LastName);
+// var LastName;
+
+// FName=4;
+// console.log(FName);
+// // let FName;
+
+// const namee=4;
+// console.log(namee);
+// // const namee;
+
+// NaNa=4;
+// console.log(NaNa);
+// var NaNa;
+
+var a =3;
+function Myfunc () {
+    var b;
+  b =5;
+  console.log(b);
+
+}
